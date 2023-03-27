4d3255f90e33f8880705d2d7a18488f891ec3d40
“pipishia” <codyhung0131@gmail.com>
2023-03-27 21:59:34 +0800

    blurrrrr

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import style from './header.module.css'
+export default function header({ title, slogan})
+{
+    return (
+        <header className={style.header}>
+            <div className="container d-flex flex-column align-items-center">
+                <img className={style.avatar} src="./images/avatar.png" alt="avatar" />
+                <h1 className={style.headerTitle}>最新消息{title}</h1>
+                <hr className={style.divider} />
+                <p className={style.slogan}>cart{slogan}</p>
+                <p className={style.slogan}>login{slogan}</p>
+            </div>
+        </header>
+    );
+}
+
