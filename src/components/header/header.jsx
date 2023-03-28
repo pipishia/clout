d2164621220cdb55ba26e8ae169dcb35f2043047
“pipishia” <codyhung0131@gmail.com>
2023-03-28 17:07:50 +0800

    ass

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,12 @@
 import style from './header.module.css'
+import { Row, Col } from 'antd';
+
 export default function header({ title, slogan})
 {
     return (
         <header className={style.header}>
             <div className="container d-flex flex-column align-items-center">
-                <img className={style.avatar} src="./images/avatar.png" alt="avatar" />
+                <img className={style.avatar} src="./images/DICK.png" alt="avatar" />
                 <h1 className={style.headerTitle}>最新消息{title}</h1>
                 <hr className={style.divider} />
                 <p className={style.slogan}>cart{slogan}</p>
