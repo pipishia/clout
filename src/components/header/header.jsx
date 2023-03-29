17c4ff004ab86505d7b60aa52af167872955a427
“pipishia” <codyhung0131@gmail.com>
2023-03-29 15:44:13 +0800

    e

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,21 @@
 import style from './header.module.css'
 import { Row, Col } from 'antd';
+import 'antd/dist/reset.css'
+import { useState } from "react";
+import CartSummary from "../CartSummary";
 
-export default function header({ title, slogan})
+
+export default function header({ title, slogan,pic})
 {
+    const [isOnTouch, setIsOnTouch] = useState(false);
     return (
         <header className={style.header}>
             <div className="container d-flex flex-column align-items-center">
                 <img className={style.avatar} src="./images/DICK.png" alt="avatar" />
                 <h1 className={style.headerTitle}>最新消息{title}</h1>
-                <hr className={style.divider} />
-                <p className={style.slogan}>cart{slogan}</p>
-                <p className={style.slogan}>login{slogan}</p>
+                <CartSummary />
+                <img className={style.pic} src="./images/cart.jpg" alt="pic"/>
+                <p className={style.slogan} src="./images/login.jfif">login{slogan}</p>
             </div>
         </header>
     );
