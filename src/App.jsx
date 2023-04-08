e146a3cc819843e7bd7bdabf28ca771f9a981e85
Victor Ho <victort509dm@gmail.com>
2023-04-08 22:24:11 +0800

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
 import 'antd/dist/reset.css';
 // import {BrowserRouter,Routes,Route} from'react react-dom' ;
 // import {ConfigProvider} from'antd';
@@ -9,12 +9,12 @@ import './App.css';
 import Activity from './pages/Activity';
 // import store from './redux/store';
 
-=======
+// =======
 import 'antd/dist/reset.css'
 import './App.css'
-import Home from './pages/home'
+// import Home from './pages/home'
 import Navbar from '../src/components/navbar/navbar'
->>>>>>> 783584aab30051951482fcf34f8e173da65b1ce8
+// >>>>>>> 783584aab30051951482fcf34f8e173da65b1ce8
 function App() {
   return (
     <Activity />
