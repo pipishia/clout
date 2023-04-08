f0f75ae82c2ba49c1c41c9347cd1ad1767a2ed27
“pipishia” <codyhung0131@gmail.com>
2023-04-08 22:41:50 +0800

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-import 'antd/dist/reset.css';
-// import {BrowserRouter,Routes,Route} from'react react-dom' ;
-// import {ConfigProvider} from'antd';
-// import{Provider}from "react-redux";
-// import{HelmetProvider} from 'reac'
-import Home from './pages/home';
-import './App.css';
-import Activity from './pages/Activity';
-// import store from './redux/store';
 
-=======
 import 'antd/dist/reset.css'
 import './App.css'
 import Home from './pages/home'
 import Navbar from '../src/components/navbar/navbar'
->>>>>>> 783584aab30051951482fcf34f8e173da65b1ce8
 function App() {
   return (
-    <Activity />
+    <Home />
 
   );
 }
