5255cad3f802885c8b10b5196b51b7583aa5b219
nick <nick920120@gmail.com>
2023-04-10 17:15:14 +0800

    edit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,19 +4,10 @@ import Masonry from "../components/masonry/masonry"
 function Home() {
     return (
         <div>
-<<<<<<< HEAD
-            <Header />
-            
-            <Text />
-            <Footer />
-            {/* <Animation/> */}
-        </div>
-=======
          <Header />
        <Masonry/>
         <Footer />
        </div>
->>>>>>> 92c5f152671e16686d74980791b4b385a05353e5
     );
 }
 
