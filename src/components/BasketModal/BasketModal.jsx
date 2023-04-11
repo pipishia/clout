e0b1f30d2a273ca441c680a259a3aa9bdca657d9
“pipishia” <codyhung0131@gmail.com>
2023-04-11 10:13:23 +0800

    edit

--- src/components/BasketModal/BasketModal.jsx
+++ src/components/BasketModal/BasketModal.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addCartItems, removeCartItems } from "../../redux/cartSlice";
 
-import styles from "./basketmodal.module.css"
+import styles from "./BasketModal.module.css"
 import { CartIcon } from "../cartsummary/icon";
 import { selectCartItems } from "../../redux/cartSlice";
 const { Option } = Select;
